67ab5626606eda6296f76b7217cbca8c0ddf8ac6
dmbcjr <dcurranjr@gmail.com>
2019-10-23 22:24:39 +0100

    Initial commit

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+  
+    public GameObject _player;
+    public float _Speed;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        CameraMove();
+
+    }
+
+    private void CameraMove()
+    {
+
+        Vector3 Target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Target.z = transform.position.z;
+
+        transform.position = Vector3.MoveTowards(transform.position, Target, _Speed * Time.deltaTime / _player.transform.localScale.x);
+    }
+}
