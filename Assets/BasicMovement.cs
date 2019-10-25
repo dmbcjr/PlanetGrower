736842b2a9a175b1653e89e0801d7f474e2840c2
dmbcjr <dcurranjr@gmail.com>
2019-10-25 03:00:05 +0100

    Revert "Revert "Initial commit""
    This reverts commit 277c425175679cc1763e413fbd0ef48444dae30a.

---
+++ Assets/BasicMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float _speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(
+            Input.GetAxis("Horizontal") * _speed,
+            Input.GetAxis("Vertical") * _speed,
+            0) * Time.deltaTime);
+    }
+}
---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float _Speed;
+
+    public GameObject _backGround;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        PlayerMove();
+
+    }
+
+    private void PlayerMove()
+    {
+
+        Vector3 Target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Target.z = transform.position.z;
+
+       // float dist = Vector2.Distance(transform.position, Target);
+       // Debug.Log(dist);
+
+        //if (dist < 2)
+        //{
+        //    _positionBarrier = 1.2f;
+
+        //}
+        //else if (dist < 1)
+        //{
+        //    _positionBarrier = 2;
+        //}
+        //else if (dist < .02)
+        //{
+        //    _positionBarrier = 10;
+        //}
+        //else if (dist < .005)
+        //{
+        //    _positionBarrier = 100;
+        //}
+        //else
+        //{
+        //    _positionBarrier = 1;
+        //}
+
+        transform.position = Vector3.MoveTowards(transform.position, Target, _Speed * Time.deltaTime / transform.localScale.x);
+
+        //_backGround.transform.position = Vector3.MoveTowards(transform.position, Target, .1f * Time.deltaTime / transform.localScale.x );
+    }
+
+   
+}
